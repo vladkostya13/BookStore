aac2f8f93f5219e4fe8a6dfb4d9cbd5c9a91e50c
v.kastyukevich <silveronxz@gmail.com>
2021-06-29 16:45:03 +0300

    add: data transfer objects

---
+++ BookStore.API/Dtos/Book/BookEditDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.API.Dtos.Book
+{
+    public class BookEditDto
+    {
+        [Key]
+        [Required(ErrorMessage = "The field {0} is required")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(150, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(150, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 2)]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(350, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 2)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        public DateTime PublishDate { get; set; }
+    }
+}
---
+++ BookStore.API/Dtos/Category/CategoryEditDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.API.Dtos.Category
+{
+    public class CategoryEditDto
+    {
+        [Key]
+        [Required(ErrorMessage = "The field {0} is required")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(150, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 2)]
+        public string Name { get; set; }
+    }
+}
